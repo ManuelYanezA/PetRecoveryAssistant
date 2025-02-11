143a42d970a9fdbf7ddcfe66e0d4620117ac86eb
manuelyanez.ar <manuelyanez.ar@gmail.com>
2025-02-11 16:05:57 -0300

    modification: deleted medCalc.jsx, made MainMenu.jsx, pending: connect MainMenu.jsx to App.jsx

---
+++ PetRecoveryAssistant-Frontend/src/components/MainMenu.jsx
@@ -0,0 +1,48 @@
+import { Link } from "react-router-dom";
+
+function MainMenu() {
+    return(
+        <div style={containerStyle}>
+            <h2 style={titleStyle}>PetRecoveryAssistant</h2>
+            <div style={buttonContainerStyle}>
+                <Link to="/calendar">
+                    <button style={buttonStyle}>Calendario de Tratamiento</button>
+                </Link>
+            </div>
+        </div>
+    );
+}
+
+const containerStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+    textAlign: "center",
+};
+
+const titleStyle = {
+    fontSize: "28px",
+    marginBottom: "20px",
+};
+
+const buttonContainerStyle = {
+    display: "flex",
+    flexDirection: "column",
+    gap: "20px",
+};
+const buttonStyle = {
+    padding: "15px 30px",
+    fontSize: "18px",
+    width: "250px",
+    borderRadius: "8px",
+    border: "none",
+    backgroundColor: "#007BFF",
+    color: "white",
+    cursor: "pointer",
+    boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+    transition: "background-color 0.3s ease",
+};
+
+export default MainMenu;
