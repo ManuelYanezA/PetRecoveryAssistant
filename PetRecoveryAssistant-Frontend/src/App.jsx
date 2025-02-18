6b086d3e22d2a0dcdcaff98469faa5c5f4286ef7
ManuelYanezA <manuelyanez.ar@gmail.com>
2025-02-18 17:08:56 -0300

    style: refactor MainMenu component to use CSS classes and remove inline styles; add styles.css

--- PetRecoveryAssistant-Frontend/src/App.jsx
+++ PetRecoveryAssistant-Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MainMenu_ele from "./components/mainmenu";
+import MainMenu_ele from "./components/mainMenu";
 import Treatment_ele from "./pages/treatment";
 import Calendar_ele from "./pages/calendar";
 
--- PetRecoveryAssistant-Frontend/src/components/MainMenu.jsx
+++ PetRecoveryAssistant-Frontend/src/components/MainMenu.jsx
@@ -1,51 +1,20 @@
 import { Link } from "react-router-dom";
+import "../styles/styles.css";
 
 function MainMenu() {
     return(
-        <div style={containerStyle}>
-            <h2 style={titleStyle}>PetRecoveryAssistant</h2>
-            <div style={buttonContainerStyle}>
+        <div className="container">
+            <h2 className="title">PetRecoveryAssistant</h2>
+            <div className="button-container">
                 <Link to="/calendar">
-                    <button style={buttonStyle}>Calendario de Tratamiento</button>
+                    <button className="button">Calendario de Tratamiento</button>
                 </Link>
                 <Link to="/treatment">
-                    <button style={buttonStyle}>Seguimiento de Tratamiento</button>
+                    <button className="button">Seguimiento de Tratamiento</button>
                 </Link>
             </div>
         </div>
     );
 }
-
-const containerStyle = {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    height: "100vh",
-    textAlign: "center",
-};
-
-const titleStyle = {
-    fontSize: "28px",
-    marginBottom: "20px",
-};
-
-const buttonContainerStyle = {
-    display: "flex",
-    flexDirection: "column",
-    gap: "20px",
-};
-const buttonStyle = {
-    padding: "15px 30px",
-    fontSize: "18px",
-    width: "250px",
-    borderRadius: "8px",
-    border: "none",
-    backgroundColor: "#007BFF",
-    color: "white",
-    cursor: "pointer",
-    boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
-    transition: "background-color 0.3s ease",
-};
 
 export default MainMenu;
