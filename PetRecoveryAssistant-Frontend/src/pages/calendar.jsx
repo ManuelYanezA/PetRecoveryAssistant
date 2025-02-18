fd4ce1113ad37070a72dfd983427dfd9685be1ed
ManuelYanezA <manuelyanez.ar@gmail.com>
2025-02-18 17:21:48 -0300

    style: refactor Calendar and Treatment pages to use CSS classes and improve layout; add styles for text and buttons

--- PetRecoveryAssistant-Frontend/src/pages/calendar.jsx
+++ PetRecoveryAssistant-Frontend/src/pages/calendar.jsx
@@ -1,13 +1,13 @@
-//import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/styles.css";
 
 const Calendar_page = () => {
     return(
-        <div style={{ padding: "20px", textAlign: "center" }}>
-            <h1>Calendario de Controles</h1>
-            <p>Esta página es para mostrar las fechas en las que se debe ir a control médico</p>
+        <div className="container">
+            <h1 className="title">Calendario de Controles</h1>
+            <p className="text-wrapper">Esta página es para mostrar las fechas en las que se debe ir a control médico</p>
             <Link to="/">
-                <button style={{ marginTop: "20px", padding: "10px 20px", cursor: "pointer" }}>
+                <button className="button">
                     Volver al menú principal
                 </button>
             </Link>
