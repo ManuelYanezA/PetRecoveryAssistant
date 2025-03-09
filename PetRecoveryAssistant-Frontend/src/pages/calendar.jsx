4545a992a3232dcbce37df2bd65f9c492f18f36a
manuelyanez.ar <manuelyanez.ar@gmail.com>
2025-03-09 19:31:07 -0300

    feature: Added react-calendar component on Calendar.jsx page

--- PetRecoveryAssistant-Frontend/src/pages/calendar.jsx
+++ PetRecoveryAssistant-Frontend/src/pages/calendar.jsx
@@ -1,11 +1,21 @@
 import { Link } from "react-router-dom";
 import "../styles/styles.css";
+import { useState } from "react";
+import Calendar from 'react-calendar';
 
 const Calendar_page = () => {
+    const [value, setValue] = useState(new Date());
+
+    function onChange(nextValue){
+        setValue(nextValue);
+    }
     return(
         <div className="container">
             <h1 className="title">Calendario de Controles</h1>
-            <p className="text-wrapper">Esta página es para mostrar las fechas en las que se debe ir a control médico</p>
+            <Calendar
+                onChange={onChange}
+                value={value}
+            />
             <Link to="/">
                 <button className="button">
                     Volver al menú principal
@@ -16,3 +26,5 @@ const Calendar_page = () => {
 };
 
 export default Calendar_page;
+
+//<p className="text-wrapper">Esta página es para mostrar las fechas en las que se debe ir a control médico</p>
